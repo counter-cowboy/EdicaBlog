fd59873aae01316674a2daeb8b6d7f40cf509df4
counter-cowboy <79208562003@ya.ru>
2024-03-12 20:51:08 +0300

    doc: show_category  9f

--- resources/views/admin/categories/show.blade.php
+++ resources/views/admin/categories/show.blade.php
@@ -8,7 +8,7 @@
             <div class="container-fluid">
                 <div class="row mb-2">
                     <div class="col-sm-6">
-                        <h1 class="m-0">Categories</h1>
+                        <h1 class="m-0">{{$category->title}}</h1>
                     </div><!-- /.col -->
                     <div class="col-sm-6">
                         <ol class="breadcrumb float-sm-right">
@@ -24,33 +24,10 @@
         <!-- Main content -->
         <section class="content">
             <div class="container-fluid">
-                <!-- Small boxes (Stat box) -->
-                <div class="row">
 
-                    <div class="col-1 mb-3">
-                        <a href="{{route('admin.category.create')}}" class="btn btn-block btn-primary">Add cat.</a>
-                    </div>
-                </div>
                 <div class="row">
                     <div class="col-6">
                         <div class="card">
-                            <div class="card-header">
-                                <h3 class="card-title">Category list</h3>
-
-                                <div class="card-tools">
-                                    <div class="input-group input-group-sm" style="width: 150px;">
-                                        <input type="text" name="table_search" class="form-control float-right"
-                                               placeholder="Search">
-
-                                        <div class="input-group-append">
-                                            <button type="submit" class="btn btn-default">
-                                                <i class="fas fa-search"></i>
-                                            </button>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <!-- /.card-header -->
                             <div class="card-body table-responsive p-0">
                                 <table class="table table-hover text-nowrap">
                                     <thead>
@@ -61,16 +38,15 @@
                                     </tr>
                                     </thead>
                                     <tbody>
-                                    @foreach($categories as $cat)
-                                        <tr>
-                                            <td>{{ $cat->id }}</td>
-                                            <td>{{$cat->title}}</td>
-                                            <td>
-                                                <a href="{{ route('admin.category.show', $cat->id) }}">
-                                                    <i class="fa fa-eye"></i></a>
-                                            </td>
-                                        </tr>
-                                    @endforeach
+
+                                    <tr>
+                                        <td>{{ $category->id }}</td>
+                                        <td>{{$category->title}}</td>
+                                        <td>
+
+                                        </td>
+                                    </tr>
+
                                     </tbody>
                                 </table>
                             </div>
