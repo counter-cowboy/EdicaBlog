421327727ae77fd2fbf43d0a3f22a064de456380
counter-cowboy <79208562003@ya.ru>
2024-04-30 16:09:30 +0300

    docs: some fix bugs

--- resources/views/admin/category/index.blade.php
+++ resources/views/admin/category/index.blade.php
@@ -1,103 +1,101 @@
 @extends('admin.layouts.main')
 @section('content')
 
-    <!-- Content Wrapper. Contains page content -->
-    <div class="content-wrapper">
-        <!-- Content Header (Page header) -->
-        <div class="content-header">
-            <div class="container-fluid">
-                <div class="row mb-2">
-                    <div class="col-sm-6">
-                        <h1 class="m-0">Categories</h1>
-                    </div><!-- /.col -->
-                    <div class="col-sm-6">
-                        <ol class="breadcrumb float-sm-right">
-                            <li class="breadcrumb-item"><a href="{{ route( 'index') }}">Main</a></li>
-                            <li class="breadcrumb-item active">Categories v1</li>
-                        </ol>
-                    </div><!-- /.col -->
-                </div><!-- /.row -->
-            </div><!-- /.container-fluid -->
-        </div>
-        <!-- /.content-header -->
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1 class="m-0">Categories</h1>
+                </div><!-- /.col -->
+                <div class="col-sm-6">
+                    <ol class="breadcrumb float-sm-right">
+                        <li class="breadcrumb-item"><a href="{{ route( 'main.index') }}">Main</a></li>
+                        <li class="breadcrumb-item active">Categories v1</li>
+                    </ol>
+                </div><!-- /.col -->
+            </div><!-- /.row -->
+        </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content-header -->
 
-        <!-- Main content -->
-        <section class="content">
-            <div class="container-fluid">
-                <!-- Small boxes (Stat box) -->
-                <div class="row">
+    <!-- Main content -->
+    <section class="content">
+        <div class="container-fluid">
+            <!-- Small boxes (Stat box) -->
+            <div class="row">
 
-                    <div class="col-1 mb-3">
-                        <a href="{{route('admin.category.create')}}" class="btn btn-block btn-primary">Add category</a>
-                    </div>
+                <div class="col-1 mb-3">
+                    <a href="{{route('admin.category.create')}}" class="btn btn-block btn-primary">Add category</a>
                 </div>
-                <div class="row">
-                    <div class="col-6">
-                        <div class="card">
-                            <div class="card-header">
-                                <h3 class="card-title">Category list</h3>
+            </div>
+            <div class="row">
+                <div class="col-6">
+                    <div class="card">
+                        <div class="card-header">
+                            <h3 class="card-title">Category list</h3>
 
-                                <div class="card-tools">
-                                    <div class="input-group input-group-sm" style="width: 150px;">
-                                        <input type="text" name="table_search" class="form-control float-right"
-                                               placeholder="Search">
+                            <div class="card-tools">
+                                <div class="input-group input-group-sm" style="width: 150px;">
+                                    <input type="text" name="table_search" class="form-control float-right"
+                                        placeholder="Search">
 
-                                        <div class="input-group-append">
-                                            <button type="submit" class="btn btn-default">
-                                                <i class="fas fa-search"></i>
-                                            </button>
-                                        </div>
+                                    <div class="input-group-append">
+                                        <button type="submit" class="btn btn-default">
+                                            <i class="fas fa-search"></i>
+                                        </button>
                                     </div>
                                 </div>
                             </div>
-                            <!-- /.card-header -->
-                            <div class="card-body table-responsive p-0">
-                                <table class="table table-hover text-nowrap">
-                                    <thead>
+                        </div>
+                        <!-- /.card-header -->
+                        <div class="card-body table-responsive p-0">
+                            <table class="table table-hover text-nowrap">
+                                <thead>
                                     <tr>
                                         <th>ID</th>
                                         <th>Name</th>
                                         <th colspan="3" class="text-center">Action</th>
                                     </tr>
-                                    </thead>
-                                    <tbody>
+                                </thead>
+                                <tbody>
                                     @foreach($categories as $cat)
-                                        <tr>
-                                            <td>{{ $cat->id }}</td>
-                                            <td>{{$cat->title}}</td>
-                                            <td class="text-center">
-                                                <a href="{{ route('admin.category.show', $cat->id) }}">
-                                                    <i class="fa fa-eye"></i></a>
-                                            </td>
-                                            <td class="text-center">
-                                                <a href="{{ route('admin.category.edit', $cat->id) }}"
-                                                   class="text-success">
-                                                    <i class="fa fa-pencil-alt"></i></a>
-                                            </td>
+                                    <tr>
+                                        <td>{{ $cat->id }}</td>
+                                        <td>{{$cat->title}}</td>
+                                        <td class="text-center">
+                                            <a href="{{ route('admin.category.show', $cat->id) }}">
+                                                <i class="fa fa-eye"></i></a>
+                                        </td>
+                                        <td class="text-center">
+                                            <a href="{{ route('admin.category.edit', $cat->id) }}" class="text-success">
+                                                <i class="fa fa-pencil-alt"></i></a>
+                                        </td>
 
-                                            <td class="text-center">
+                                        <td class="text-center">
 
-                                                <form action="{{ route('admin.category.delete', $cat->id) }}"
-                                                      method="post">
-                                                    @csrf
-                                                    @method('delete')
-                                                    <button type="submit" class="border-0 bg-transparent">
-                                                        <i role="button" onclick="return confirm('Sure?')"
-                                                           class="fa fa-trash text-danger"></i>
-                                                    </button>
-                                                </form>
-                                            </td>
-                                        </tr>
+                                            <form action="{{ route('admin.category.delete', $cat->id) }}" method="post">
+                                                @csrf
+                                                @method('delete')
+                                                <button type="submit" class="border-0 bg-transparent">
+                                                    <i role="button" onclick="return confirm('Sure?')"
+                                                        class="fa fa-trash text-danger"></i>
+                                                </button>
+                                            </form>
+                                        </td>
+                                    </tr>
                                     @endforeach
-                                    </tbody>
-                                </table>
-                            </div>
+                                </tbody>
+                            </table>
                         </div>
                     </div>
-
                 </div>
+
             </div>
-        </section>
-    </div>
+        </div>
+    </section>
+</div>
 
 @endsection
--- resources/views/admin/user/edit.blade.php
+++ resources/views/admin/user/edit.blade.php
@@ -1,84 +1,81 @@
 @extends('admin.layouts.main')
 @section('content')
 
-    <div class="content-wrapper">
-        <div class="content-header">
-            <div class="container-fluid">
-                <div class="row mb-2">
-                    <div class="col-sm-6">
-                        <h1 class="m-0">Editing user</h1>
-                        {{--Delete user button--}}
-                        <form action="{{ route('admin.user.delete', $user->id) }}"
-                              method="post">
-                            @csrf
-                            @method('delete')
-                            <button type="submit" class="border-0 bg-transparent">
-                                <i role="button" onclick="return confirm('Sure?')"
-                                   class="fa fa-trash text-danger"></i>
-                            </button>
-                        </form>
-                    </div>
-                    <div class="col-sm-6">
-                        <ol class="breadcrumb float-sm-right">
-                            <li class="breadcrumb-item"><a href="{{ route( 'index') }}">Main</a></li>
-                            <li class="breadcrumb-item "><a href="{{route( 'admin.user.index')}}">Users v1</a></li>
-                            <li class="breadcrumb-item active">{{$user->name}}</li>
-                        </ol>
-                    </div>
+<div class="content-wrapper">
+    <div class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1 class="m-0">Editing user</h1>
+                    {{--Delete user button--}}
+                    <form action="{{ route('admin.user.delete', $user->id) }}" method="post">
+                        @csrf
+                        @method('delete')
+                        <button type="submit" class="border-0 bg-transparent">
+                            <i role="button" onclick="return confirm('Sure?')" class="fa fa-trash text-danger"></i>
+                        </button>
+                    </form>
+                </div>
+                <div class="col-sm-6">
+                    <ol class="breadcrumb float-sm-right">
+                        <li class="breadcrumb-item"><a href="{{ route( 'main.index') }}">Main</a></li>
+                        <li class="breadcrumb-item "><a href="{{route( 'admin.user.index')}}">Users v1</a></li>
+                        <li class="breadcrumb-item active">{{$user->name}}</li>
+                    </ol>
                 </div>
             </div>
         </div>
+    </div>
 
-        <section class="content">
-            <div class="container-fluid">
-                <div class="row">
-                    <div class="col-12">
-                        <form action="{{ route('admin.user.update', $user->id) }}" method="post" class="w-25">
-                            @csrf
-                            @method('patch')
-                            {{--Name--}}
-                            <div class="form-group">
-                                <input type="text" name="name" class="form-control"
-                                       value="{{$user->name}}" placeholder="Enter username">
-                                @error('name')
-                                <div class="text-danger">{{$message}} </div>
-                                @enderror
-                            </div>
+    <section class="content">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12">
+                    <form action="{{ route('admin.user.update', $user->id) }}" method="post" class="w-25">
+                        @csrf
+                        @method('patch')
+                        {{--Name--}}
+                        <div class="form-group">
+                            <input type="text" name="name" class="form-control" value="{{$user->name}}"
+                                placeholder="Enter username">
+                            @error('name')
+                            <div class="text-danger">{{$message}} </div>
+                            @enderror
+                        </div>
 
-                            {{--EMAIL User email--}}
-                            <div class="form-group">
-                                <input type="text" name="email" class="form-control" placeholder="Enter email"
-                                       value="{{$user->email}}">
-                                @error('email')
-                                <div class="text-danger">{{$message}} </div>
-                                @enderror
-                            </div>
-                            {{-- Select user role--}}
-                            <div class="form-group w-25">
-                                <label>Select role</label>
-                                <select name="role" class="form-control">
-                                    @foreach($roles as $id => $role)
-                                        <option value="{{$id}}"
-                                                {{$id == $user->role? ' selected' : ''}}>
-                                            {{$role}}
-                                        </option>
-                                    @endforeach
-                                </select>
-                                @error('role')
-                                <div class="text-danger">{{ $message }}  </div>
-                                @enderror
-                            </div>
-                            {{-- Hidden field--}}
-                            <div class="form-group">
-                                <input type="hidden" name="user_id" value="{{$user->id}}">
-                            </div>
-                            {{--Input button--}}
-                            <input type="submit" class="btn btn-primary col-4" value="Update">
-                        </form>
+                        {{--EMAIL User email--}}
+                        <div class="form-group">
+                            <input type="text" name="email" class="form-control" placeholder="Enter email"
+                                value="{{$user->email}}">
+                            @error('email')
+                            <div class="text-danger">{{$message}} </div>
+                            @enderror
+                        </div>
+                        {{-- Select user role--}}
+                        <div class="form-group w-25">
+                            <label>Select role</label>
+                            <select name="role" class="form-control">
+                                @foreach($roles as $id => $role)
+                                <option value="{{$id}}" {{$id == $user->role? ' selected' : ''}}>
+                                    {{$role}}
+                                </option>
+                                @endforeach
+                            </select>
+                            @error('role')
+                            <div class="text-danger">{{ $message }} </div>
+                            @enderror
+                        </div>
+                        {{-- Hidden field--}}
+                        <div class="form-group">
+                            <input type="hidden" name="user_id" value="{{$user->id}}">
+                        </div>
+                        {{--Input button--}}
+                        <input type="submit" class="btn btn-primary col-4" value="Update">
+                    </form>
 
-                    </div>
                 </div>
             </div>
-        </section>
-    </div>
+        </div>
+    </section>
+</div>
 @endsection
